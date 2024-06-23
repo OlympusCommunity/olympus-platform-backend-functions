30f69127bd590a221e6c0c6301cea7515e40f672
Julius Sauco <julius.sauco@gmail.com>
2024-06-22 22:10:16 -0500

    first commit

---
+++ src/test/java/com/olympus/OlympusApplicationTests.java
@@ -0,0 +1,15 @@
+package com.olympus;
+
+import net.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlympusApplicationTests {
+
+	@Test
+	@IgnoreForBinding
+	void contextLoads() {
+	}
+
+}
